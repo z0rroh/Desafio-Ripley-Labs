5ef9624d00ac2be5ddbc7cb1c19e42b9933fa6b3
Carlos Riquelme Labrin <caarlos@live.cl>
2019-10-03 00:11:58 -0300

    add env vars

--- client/src/components/CatalogProduct/ProductLogo/ProductLogo.js
+++ client/src/components/CatalogProduct/ProductLogo/ProductLogo.js
@@ -2,15 +2,17 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 import './ProductLogo.css'
 
+const home_ripley = process.env.REACT_APP_HOME_RIPLEY
+const proxyurl = process.env.REACT_APP_PROXY_URL //habilita las cors para poder realizar la peticion http
+
 const ProductLogo = ({ brand }) => {
    const brandName = brand.value;
    const [hasLogo, setHasLogo] = useState(false)
-   
+
    useEffect(() => {
       const checkLogo = async() => {
          try{
-            const proxyurl = "https://cors-anywhere.herokuapp.com/";//habilita las cors para poder realizar la peticion http
-            await axios.get(`${proxyurl}https://home.ripley.cl/brand-logos/${brandName.toLowerCase()}.svg`)
+            await axios.get(`${proxyurl}${home_ripley}${brandName.toLowerCase()}.svg`)
             setHasLogo(true)
          }catch(e){
             setHasLogo(false)
