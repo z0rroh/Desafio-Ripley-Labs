1d38ab6d4e0bd9e3f0e1882fe0946fb471e72e06
Carlos Riquelme Labrin <caarlos@live.cl>
2019-10-08 17:52:44 -0300

    change ENV prod vars

--- server/index.js
+++ server/index.js
@@ -6,16 +6,7 @@ const config = {
   port: process.env.SERVER_PORT,
 }
 
-const normalizePort = (val) => {
-  const portNumber = parseInt(val, 10);
-  if (isNaN(portNumber)) return val;
-  if (portNumber >= 0) return portNumber;
-  return false;
-}
 
-const port = normalizePort(config.port);
-api.set('port', port);
-
-api.listen(port, () => {
+api.listen(config.port, () => {
    console.log('Desafio Ripley API - API Running on port: ' + config.port)
 })
